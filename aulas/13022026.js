4cabee4ec2e349d0769d63d7009752edf9dcf47b
luisjames579 <luis.sasdelli@escola.pr.gov.br>
2026-03-19 07:55:55 -0300

    update

---
+++ aulas/13022026.js
@@ -0,0 +1,32 @@
+let lucro =30;
+let lucro = 10;
+
+function soma(a,b){
+    return a + b
+}
+soma(lucro,lucro2)
+
+function resultadoDivisaoSocio(lucro,lucro2){
+       return soma(lucro,lucro2)/2
+}
+
+function calculadora(ari,a,b){
+    switch(ari){
+        case '+':
+            resultado = a + b;
+            break;
+        case '-':
+            resultado = a - b;
+            break;
+        case '*':
+            resultado = a * b;
+            break;
+        case '/':
+            resultado = a / b;
+            break;
+            default:
+                resultado = 'Opoerador Inválido';
+    }
+    return resultado
+
+}
