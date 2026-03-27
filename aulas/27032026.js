2ffab6679c88c590cebb0547c7fad449a66f3955
luisjames579 <luis.sasdelli@escola.pr.gov.br>
2026-03-27 07:57:03 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,14 @@
+// Criar Arry
+
+const fila = [1,2,3,4,5,6];
+
+const pfilaSotrigAmigos = ["Ana Julia","Bob","Lebron"]
+ 
+function LerAmigos(filaStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length} amigo`) 
+
+    for(let i = 0; i < filaStringAmigos.length;i++){
+       console.log(FilaStringAmigos[])
+
+}
+
